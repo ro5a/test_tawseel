d0e51e7eb0f5884d76b8b9aa0a400e01f135febd
roaa-hakimi <108170193+roaa-hakimi@users.noreply.github.com>
2024-12-04 13:32:17 +0300

    add resource

--- app/Http/Controllers/Api/Auth/UserController.php
+++ app/Http/Controllers/Api/Auth/UserController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers\Api\Auth;
 
 use App\Http\Controllers\Controller;
+use App\Http\Resources\UserResource;
 use App\Models\User;
 
 class UserController extends Controller
@@ -10,7 +11,7 @@ class UserController extends Controller
     public function getData()
     {
 
-        $users = User::get();
+        $users = UserResource::collection(User::orderBy('id', 'DESC')->get());
 
         return response()->json([
             'users' => $users,
